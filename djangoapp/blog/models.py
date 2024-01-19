2be07a7fd337186cae451a37c4225d94bd3c06ec
Leandro-rodrigues-silva-miranda <leandrorsmlp@hotmail.com>
2024-01-19 12:36:32 -0300

    Models creations: Page and Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -18,6 +18,9 @@ class Tag(models.Model):
             self.slug = new_slugified(self.name)
         return super().save(*args, **kwargs)
     
+    def __str__(self) -> str:
+        return self.name
+    
 
 class Category(models.Model):
     class Meta:
@@ -35,3 +38,61 @@ class Category(models.Model):
         if not self.slug:
             self.slug = new_slugified(self.title)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title 
+    
+class Page(models.Model):
+    
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(
+        unique=True,default=None,
+        null=False,blank=True,max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text = '"Está publicado", se o campo estiver marcado, esta página será exibida'
+        )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugified(self.title)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,default=None,
+        null=False,blank=True,max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text = '"Está publicado", se o campo estiver marcado, este post será exibida'
+        )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m',blank=True,default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text = 'Exibir imagem de capa no conteúdo do post?'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True,default=None)
+    tag = models.ManyToManyField(Tag,blank=True,default='')
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugified(self.title)
+        return super().save(*args, **kwargs)    
